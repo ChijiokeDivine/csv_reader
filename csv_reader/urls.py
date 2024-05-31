f58cd33241564010a97282ad1a5e3bb83979111d
Chijioke Divine <108485884+ChijiokeDivine@users.noreply.github.com>
2024-05-31 13:52:27 +0100

    made some changes

--- csv_reader/urls.py
+++ csv_reader/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.urls import re_path as url
+from django.views.static import serve
+
 from django.conf.urls.static import static
 urlpatterns = [
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path("admin/", admin.site.urls),
     path("", include('core.urls')),
 ]
